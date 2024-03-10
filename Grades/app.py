02d6d8c98d63068a363fe6336206037cb319e881
riley <20026@wc.school.nz>
2024-03-10 13:38:01 +1300

    Version 3

--- Grades/app.py
+++ Grades/app.py
@@ -1,6 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
+import sqlite3
+from sqlite3 import Error
 
+DATABASE = 'C:/Users/riley/OneDrive/DTS/13DTS/Flask/gradesProject/Grades/grades.db'
 app = Flask(__name__)
+
+
+def create_connection(db_file):
+    try:
+        connection = sqlite3.connect(db_file)
+        return connection
+    except Error as e:
+        print(e)
+    return None
 
 
 @app.route('/')
