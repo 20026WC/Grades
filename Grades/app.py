b5c371c756160506f1ea84b46f8af6f2f1a63c34
riley <20026@wc.school.nz>
2024-03-10 14:44:13 +1300

    Version 9

--- Grades/app.py
+++ Grades/app.py
@@ -22,7 +22,14 @@ def render_homepage():  # put application's code here
 
 @app.route('/student')
 def render_student_page():  # put application's code here
-    return render_template('student.html')
+    con = create_connection(DATABASE)
+    query = "SELECT id, name, last_name, year_level, email, description FROM Student"
+    cur = con.cursor()
+    cur.execute(query)
+    student_list = cur.fetchall()
+    con.close()
+    print(student_list)
+    return render_template('student.html', students=student_list)
 
 
 if __name__ == '__main__':
